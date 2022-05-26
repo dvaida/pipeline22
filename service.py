d4a37014ad4939f53238a19d04d213b67ea90e6c
Dorel Vaida <dorel.vaida@adaptiv.software>
2022-05-26 15:55:22 +0300

    Use port ENV var, move tree to env var as well

--- service.py
+++ service.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 
 @app.get("/tree")
 def tree():
-    return {"favourite_tree": "pine"}
+    tree_name = os.environ.get("TREE", "pine")
+    return {"favourite_tree": tree_name}
